720e32b3b374b5047272de9b14b8bb0f09c29c0e
NAMRATAGAWANDE <nnammu35@gmail.com>
2024-06-08 17:43:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ import pandas as pd
 st.title("Zena's Amazing Athleisure Catalog")
 
 # Connect to Snowflake
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cnx = stconnection("snowflake")
+session=my_cnx.session()
 my_cur = my_cnx.cursor()
 
 # Run a Snowflake query
